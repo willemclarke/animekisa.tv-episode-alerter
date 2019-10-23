e51fea9be260a676546cd2de6fcb2705c9c7907a
Willem Clarke <willem.2@hotmail.com>
2019-10-23 16:16:25 +1000

    working with npmcheerior to scrape episode data

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const axios = require("axios");
+const cheerio = require("cheerio");
 
-const url = "https://kissanime.ru/Anime/Dr-Stone";
+const url = "https://animekisa.tv/dr-stone";
 
 axios
+  // fetching html page data
   .get(url)
   .then(response => {
     console.log(response.data);
+
+    //scraping list of episodes
+    let getData = html => {
+      data = [];
+      const $ = cheerio.load(html);
+      $("div.infoept2 div.centerv").each((i, elem) => {
+        data.push({
+          episode: $(elem).text()
+        });
+      });
+      console.log(data);
+    };
+
+    getData(response.data);
   })
+
+  //handling errors
   .catch(error => {
     console.log(error);
   });
