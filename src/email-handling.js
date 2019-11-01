4876ad0aa9556e55efcc95d861d469c103d8a3a2
Willem Clarke <willem.2@hotmail.com>
2019-11-01 12:48:29 +1000

    housekeeping, created src folder to store index.js & email-handling.js

---
+++ src/email-handling.js
@@ -0,0 +1,35 @@
+const nodemailer = require("nodemailer");
+
+class Email {
+  sendEmail(episodesForEmail, name) {
+    const transporter = nodemailer.createTransport({
+      host: "smtp-mail.outlook.com", // hostname
+      secureConnection: false, // TLS requires secureConnection to be false
+      port: 587, // port for secure SMTP
+      tls: {
+        ciphers: "SSLv3"
+      },
+      auth: {
+        user: "placeholder@outlook.com",
+        pass: "placeholderpass@outlook.com"
+      }
+    });
+
+    const mailOptions = {
+      from: '"Anime Alerter " <placeholder@outlook.com>', // sender address (who sends)
+      to: "placeholder@outlook.com", // list of receivers (who receives)
+      subject: `${name} New Episode Alert!`, // Subject line
+      text: `${name} has ${episodesForEmail} new episodes released on platforms such as animekisa.tv and kissanime.ru` // plaintext body
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
+      if (error) {
+        return console.log(error);
+      }
+
+      console.log("Message sent: " + info.response);
+    });
+  }
+}
+
+module.exports = Email;
